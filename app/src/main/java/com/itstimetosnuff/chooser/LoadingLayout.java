605ae2937580f56e12cb7fb01dd63dfa5d771f22
Ivan Stelmakh <itstimetosnuff@gmail.com>
2020-10-20 12:39:52 +0300

    fix onPause

--- app/src/main/java/com/itstimetosnuff/chooser/LoadingLayout.java
+++ app/src/main/java/com/itstimetosnuff/chooser/LoadingLayout.java
@@ -10,17 +10,28 @@ import java.util.TimerTask;
 
 public class LoadingLayout extends AppCompatActivity {
 
+    TimerTask timerTask;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loading_layout);
 
-        new Timer().schedule(new TimerTask() {
+        timerTask = new TimerTask() {
             @Override
             public void run() {
                 startActivity(new Intent(LoadingLayout.this, MainActivity.class));
                 finish();
             }
-        }, 3000);
+        };
+
+        new Timer().schedule(timerTask, 3000);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        timerTask.cancel();
     }
 }
